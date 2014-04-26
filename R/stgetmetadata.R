1b495f7b6c8e2b994e08eac99508d8bf3dd86db1
Mikael Bourqui <mejb@users.noreply.github.com>
2014-04-27 08:52:53 +0900

    stfind() now includes RESOURCE_ID field.

--- R/stgetmetadata.R
+++ R/stgetmetadata.R
@@ -1,21 +1,24 @@
-#' @name stgetmetadata
 #' @title Obtain the metadata of a table or other resource available
 #' in the Japanese official statistics open-data access system.
 #' 
-#' @param resource.id Numerical code of the resource to look up
-#' metadata for.
-#' @param lang Language of labels and values (ISO-639-1 code).
-#' @return An \code{st_metadata}-class list of \code{table.data} and
-#' \code{classes}, both data frames.
+#' @param resource.id Numerical code of the resource, typically a
+#' table, to look up metadata for.
+#' @param lang Language for labels and values (ISO-639-1 code).
+#' @return A list of \code{table}, a list of information about the
+#' table, and \code{classes}, a data frame of the class (category)
+#' codes and names used in the table.
 #' @export
 stgetmetadata <- function(resource.id, lang=NA) {
     id     <- format_resource_id(resource.id)
     params <- list(statsDataId = id)
     params <- add_lang_option(params, lang)
-    parse_metadata(
+    res <- parse_metadata(
         xmlRoot(
             nstac_api_call("getMetaInfo", params)
             )[['METADATA_INF']]
         )
+    res$table$RESOURCE_ID <- resource.id
+
+    res
 }
 ## ST統計表メタ情報 <- function(表コード, 言語=NA) stgetmetadata(表コード, 言語)
